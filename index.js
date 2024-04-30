f8d8acd8b7426899cb4d5c083b5e6e78a46a2754
Bithe <farzana.hossain5@gmail.com>
2024-04-30 11:49:56 +0600

    cors confiq added

--- index.js
+++ index.js
@@ -10,7 +10,14 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 //MIDDLEWARE
-app.use(cors());
+// app.use(cors());
+const corsConfig = {
+  origin: '*',
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE']
+  }
+  app.use(cors(corsConfig))
+
 app.use(express.json());
 
 console.log(process.env.DB_USER);
